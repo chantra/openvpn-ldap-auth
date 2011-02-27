db89d467c2e94eba96a5bd3e76eea13f54097575
chantra <chantra@debuntu.org>
2011-02-27 21:20:07 +0100

    str_replace_all: Fix infinite loop
    When the replacement string is part of the needle,
    the while loop never exit.
    ex: str_replace_all("foobar", "foo", "foo2");
    
    The plugin was not affected as we only replace_all "\\n" by "\n"

--- src/utils.c
+++ src/utils.c
@@ -119,12 +119,14 @@ str_replace_all ( const char *string, const char *substr, const char *replacemen
   char *tok = NULL;
   char *newstr = NULL;
   char *oldstr = NULL;
+  char *head = NULL;
 
   /* if either substr or replacement is NULL, duplicate string a let caller
  * handle it */
   if ( substr == NULL || replacement == NULL ) return strdup (string);
   newstr = strdup (string);
-  while ( (tok = strstr ( newstr, substr ))){
+  head = newstr;
+  while ( (tok = strstr ( head, substr ))){
     oldstr = newstr;
     newstr = malloc ( strlen ( oldstr ) - strlen ( substr ) + strlen ( replacement ) + 1 );
     /*failed to alloc mem, free old string and return NULL */
@@ -136,6 +138,8 @@ str_replace_all ( const char *string, const char *substr, const char *replacemen
     memcpy ( newstr + (tok - oldstr), replacement, strlen ( replacement ) );
     memcpy ( newstr + (tok - oldstr) + strlen( replacement ), tok + strlen ( substr ), strlen ( oldstr ) - strlen ( substr ) - ( tok - oldstr ) );
     memset ( newstr + strlen ( oldstr ) - strlen ( substr ) + strlen ( replacement ) , 0, 1 );
+    /* move back head right after the last replacement */
+    head = newstr + (tok - oldstr) + strlen( replacement );
     free (oldstr);
   }
   return newstr;
